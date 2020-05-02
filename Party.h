c7a23200f7203065212a29f8ecc97d22b50d02fc
OkonomiSource <username@example.com>
2020-05-02 22:22:56 +0900

    Refactoring

---
+++ Party.h
@@ -0,0 +1,36 @@
+#pragma once
+#include"Character.h"
+#define MAX_PARTY_MEM 4
+
+// パーティクラス
+class CParty
+{
+private:
+
+	// パーティ
+	// 各要素にキャラクターが入る
+	// 最大人数はMAX_PARTY_MEM まで。
+	CCharacter aCMember[MAX_PARTY_MEM] = {};
+
+	// パーティの人数
+	unsigned int MemberOfParty = 0;
+
+protected:
+public:
+
+private:
+protected:
+public:
+
+	CParty();
+	~CParty();
+
+	// パーティのメンバーを設定
+	bool SetMember(unsigned int iNum, unsigned int iChara);
+
+	// パーティのメンバーを取得
+	CCharacter GetMember(unsigned int iNum);
+
+	// パーティの人数を取得
+	unsigned int GetMemberOfParty();
+};
